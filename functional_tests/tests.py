36e27c374c3aa5e1ba12ac0cf40f8e4f74da5b71
Tong Wu <tongwu@live.com>
2019-07-11 01:09:59 -0700

    Shorten test explicit wait time

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 import time
 
-MAX_WAIT = 10
+MAX_WAIT = 5
 
 
 class NewVisitorTest(LiveServerTestCase):
