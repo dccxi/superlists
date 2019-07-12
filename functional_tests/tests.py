520f645c163b04533e0fd6e8b01e586ac11fa758
Tong Wu <tongwu@live.com>
2019-07-12 09:14:13 -0700

    Fix typo

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -96,7 +96,7 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Francis gets his own unique URL
         francis_list_url = self.browser.current_url
-        self.assertRegex(francis_list_url, "/list/.+")
+        self.assertRegex(francis_list_url, "/lists/.+/")
         self.assertNotEqual(francis_list_url, edith_list_url)
 
         # Again, there is not trace of Edith's list
