345bd68f7573c573ba4aab00c24a7cf90c8b9c50
Tong Wu <tongwu@live.com>
2019-07-20 21:47:27 -0700

    use form in all views, back to working state

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.utils.html import escape
 from lists.models import Item, List
-from lists.forms import ItemForm
+from lists.forms import ItemForm, EMPTY_ITEM_ERROR
 
 
 class HomePageTest(TestCase):
@@ -77,13 +77,21 @@ class ListViewTest(TestCase):
 
         self.assertRedirects(response, f"/lists/{correct_list.id}/")
 
-    def test_validation_errors_are_sent_back_to_home_page_template(self):
+    def test_for_invalid_input_renders_home_template(self):
         response = self.client.post("/lists/new", data={"text": ""})
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "home.html")
-        expected_error = escape("You can't have an empty list item")
-        self.assertContains(response, expected_error)
+
+    def test_validation_errors_are_shown_on_home_page(self):
+        response = self.client.post("/lists/new", data={"text": ""})
+
+        self.assertContains(response, escape(EMPTY_ITEM_ERROR))
+
+    def test_for_invalid_input_passes_form_to_template(self):
+        response = self.client.post("/lists/new", data={"text": ""})
+
+        self.assertIsInstance(response.context["form"], ItemForm)
 
     def test_invalid_list_items_are_not_save(self):
         self.client.post("/lists/new", data={"text": ""})
@@ -93,8 +101,38 @@ class ListViewTest(TestCase):
 
     def test_validation_errors_end_up_on_lists_page(self):
         list_ = List.objects.create()
+
         response = self.client.post(f"/lists/{list_.id}/", data={"text": ""})
+
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "list.html")
         expected_error = escape("You can't have an empty list item")
         self.assertContains(response, expected_error)
+
+    def test_displays_item_form(self):
+        list_ = List.objects.create()
+        response = self.client.get(f"/lists/{list_.id}/")
+
+        self.assertIsInstance(response.context["form"], ItemForm)
+        self.assertContains(response, 'name="text"')
+
+    def post_invalid_input(self):
+        list_ = List.objects.create()
+        return self.client.post(f"/lists/{list_.id}/", data={"text": ""})
+
+    def test_for_invalid_input_nothing_saved_to_db(self):
+        self.post_invalid_input()
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_for_invalid_input_renders_list_template(self):
+        response = self.post_invalid_input()
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "list.html")
+
+    def test_for_invalid_input_passes_form_to_template(self):
+        response = self.post_invalid_input()
+        self.assertIsInstance(response.context["form"], ItemForm)
+
+    def test_for_invalid_input_show_error_on_page(self):
+        response = self.post_invalid_input()
+        self.assertContains(response, escape(EMPTY_ITEM_ERROR))
--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect
-from django.core.exceptions import ValidationError
 from lists.models import Item, List
 from lists.forms import ItemForm
 
@@ -10,25 +9,19 @@ def home_page(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    error = None
+    form = ItemForm()
     if request.method == "POST":
-        try:
-            item = Item(text=request.POST["text"], list=list_)
-            item.full_clean()
-            item.save()
+        form = ItemForm(data=request.POST)
+        if form.is_valid():
+            Item.objects.create(text=request.POST["text"], list=list_)
             return redirect(list_)
-        except ValidationError:
-            error = "You can't have an empty list item"
-    return render(request, "list.html", {"list": list_, "error": error})
+    return render(request, "list.html", {"list": list_, "form": form})
 
 
 def new_list(request):
-    list_ = List.objects.create()
-    item = Item.objects.create(text=request.POST["text"], list=list_)
-    try:
-        item.full_clean()
-    except ValidationError:
-        list_.delete()
-        error = "You can't have an empty list item"
-        return render(request, "home.html", {"error": error})
-    return redirect(list_)
+    form = ItemForm(data=request.POST)
+    if form.is_valid():
+        list_ = List.objects.create()
+        Item.objects.create(text=request.POST["text"], list=list_)
+        return redirect(list_)
+    return render(request, "home.html", {"form": form})
