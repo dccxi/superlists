7619fc57ec60a2b4b24aabbf1a8a691e275cdd9c
Tong Wu <tongwu@live.com>
2019-07-20 17:35:21 -0700

    use new form in home_page, simplify test. N.B. breaks stuff

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,12 +1,17 @@
 from django.test import TestCase
 from django.utils.html import escape
 from lists.models import Item, List
+from lists.forms import ItemForm
 
 
 class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get("/")
         self.assertTemplateUsed(response, "home.html")
+
+    def test_home_page_uses_item_form(self):
+        response = self.client.get("/")
+        self.assertIsInstance(response.context["form"], ItemForm)
 
 
 class ListViewTest(TestCase):
@@ -81,7 +86,7 @@ class ListViewTest(TestCase):
         self.assertContains(response, expected_error)
 
     def test_invalid_list_items_are_not_save(self):
-        self.client.post('/lists/new', data={"item_text": ""})
+        self.client.post("/lists/new", data={"item_text": ""})
 
         self.assertEqual(List.objects.count(), 0)
         self.assertEqual(Item.objects.count(), 0)
