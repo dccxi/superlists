63933b2b0aa8c7c455354b7977054b33ce8a6616
Tong Wu <tongwu@live.com>
2019-07-22 08:35:13 -0700

    use form instead of model to save

--- lists/views.py
+++ lists/views.py
@@ -22,6 +22,6 @@ def new_list(request):
     form = ItemForm(data=request.POST)
     if form.is_valid():
         list_ = List.objects.create()
-        Item.objects.create(text=request.POST["text"], list=list_)
+        form.save(for_list=list_)
         return redirect(list_)
     return render(request, "home.html", {"form": form})
