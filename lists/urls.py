8c7c576184b19872012ca81168fd24c28ed3cd90
Tong Wu <tongwu@live.com>
2019-07-23 19:20:03 -0700

    Refactor view to use Class-Based Generic Views

--- lists/urls.py
+++ lists/urls.py
@@ -18,6 +18,6 @@ from lists import views
 
 
 urlpatterns = [
-    url(r"^new$", views.new_list, name="new_list"),
-    url(r"^(\d+)/$", views.view_list, name="view_list"),
+    url(r"^new$", views.NewListView.as_view(), name="new_list"),
+    url(r"^(?P<pk>\d+)/$", views.ViewAndAddToList.as_view(), name="view_list"),
 ]
--- lists/views.py
+++ lists/views.py
@@ -1,27 +1,30 @@
 from django.shortcuts import render, redirect
+from django.views.generic import FormView, CreateView, DetailView, DetailView
 from lists.models import List
 from lists.forms import ItemForm, ExistingListItemForm
 
 
-def home_page(request):
-    return render(request, "home.html", {"form": ItemForm()})
+class HomePageView(FormView):
+    template_name = "home.html"
+    form_class = ItemForm
 
 
-def view_list(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    form = ExistingListItemForm(for_list=list_)
-    if request.method == "POST":
-        form = ExistingListItemForm(for_list=list_, data=request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect(list_)
-    return render(request, "list.html", {"list": list_, "form": form})
+class ViewAndAddToList(DetailView, CreateView):
+    template_name = "list.html"
+    model = List
+    form_class = ExistingListItemForm
+
+    def get_form(self):
+        return self.form_class(
+            for_list=self.get_object(), data=self.request.POST
+        )
 
 
-def new_list(request):
-    form = ItemForm(data=request.POST)
-    if form.is_valid():
+class NewListView(CreateView):
+    template_name = "home.html"
+    form_class = ItemForm
+
+    def form_valid(self, form):
         list_ = List.objects.create()
         form.save(for_list=list_)
         return redirect(list_)
-    return render(request, "home.html", {"form": form})
