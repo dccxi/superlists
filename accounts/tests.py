3db5c848a45174d3d4db3c53cee049061dd77d74
Tong Wu <tongwu@live.com>
2019-08-09 08:04:43 -0700

    accounts model layer

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from accounts.models import Token
 
-# Create your tests here.
+User = get_user_model()
+
+class UserModeTest(TestCase):
+    def test_user_is_valid_with_email_only(self):
+        user = User(email="a@b.com")
+        user.full_clean()  # should not raise
+
+    def test_email_is_primary_key(self):
+        user = User(email="a@b.com")
+        self.assertEqual(user.pk, "a@b.com")
+
+class TokenModelTest(TestCase):
+    def test_links_user_with_auto_generated_uid(self):
+        token1 = Token.objects.create(email="a@b.com")
+        token2 = Token.objects.create(email="a@b.com")
+        self.assertNotEqual(token1.uid, token2.uid)
