ad79d53537dacb0979710cc4d8f76412739219c0
Tong Wu <tongwu@live.com>
2019-08-13 15:48:08 -0700

    adding trailing slash

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -159,5 +159,5 @@ class ListViewTest(TestCase):
 
 class MyListsTest(TestCase):
     def test_my_lists_url_renders_my_lists_template(self):
-        response = self.client.get("/lists/users/a@b.com")
+        response = self.client.get("/lists/users/a@b.com/")
         self.assertTemplateUsed(response, "my_lists.html")
