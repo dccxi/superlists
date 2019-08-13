bf21934392543c7665285566b817285c0460605d
Tong Wu <tongwu@live.com>
2019-08-12 22:14:55 -0700

    remove unused imports

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -1,11 +1,7 @@
 from django.conf import settings
-from django.contrib.auth import BACKEND_SESSION_KEY, SESSION_KEY, get_user_model
-from django.contrib.sessions.backends.db import SessionStore
 from .base import FunctionalTest
 from .server_tools import create_session_on_server
 from .management.commands.create_session import create_pre_authenticated_session
-
-User = get_user_model()
 
 
 class MyListsTest(FunctionalTest):
@@ -19,11 +15,7 @@ class MyListsTest(FunctionalTest):
         ## 404 pages load the quickest
         self.browser.get(self.live_server_url + "/404_no_such_url/")
         self.browser.add_cookie(
-            dict(
-                name=settings.SESSION_COOKIE_NAME,
-                value=session_key,
-                path="/",
-            )
+            dict(name=settings.SESSION_COOKIE_NAME, value=session_key, path="/")
         )
 
     def test_logged_in_users_lists_are_saved_as_my_lists(self):
