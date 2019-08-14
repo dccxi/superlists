42c9480896dd02f14bc41ba472953625cde7e22b
Tong Wu <tongwu@live.com>
2019-08-13 19:24:55 -0700

    new_list view tries to assign owner but can't

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.utils.html import escape
+from django.contrib.auth import get_user_model
 from lists.models import Item, List
 from lists.forms import (
     ItemForm,
@@ -7,6 +8,8 @@ from lists.forms import (
     EMPTY_ITEM_ERROR,
     DUPLICATE_ITEM_ERROR,
 )
+
+User = get_user_model()
 
 
 class HomePageTest(TestCase):
@@ -55,6 +58,12 @@ class NewListView(TestCase):
         self.assertEqual(List.objects.count(), 0)
         self.assertEqual(Item.objects.count(), 0)
 
+    def test_list_owner_is_saved_if_user_is_authenticated(self):
+        user = User.objects.create(email="a@b.com")
+        self.client.force_login(user)
+        self.client.post("/lists/new", data={"text": "new item"})
+        lists_ = List.objects.first()
+        self.assertEqual(lists_.owner, user)
 
 class ListViewTest(TestCase):
     def test_uses_list_template(self):
@@ -161,5 +170,12 @@ class ListViewTest(TestCase):
 
 class MyListsTest(TestCase):
     def test_my_lists_url_renders_my_lists_template(self):
+        User.objects.create(email="a@b.com")
         response = self.client.get("/lists/users/a@b.com/")
         self.assertTemplateUsed(response, "my_lists.html")
+
+    def test_passes_correct_owner_to_template(self):
+        User.objects.create(email="wrong@owner.com")
+        correct_user = User.objects.create(email="a@b.com")
+        response = self.client.get("/lists/users/a@b.com/")
+        self.assertEqual(response.context["owner"], correct_user)
--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import redirect, render
 from django.views.generic import FormView, CreateView, DetailView
+from django.contrib.auth import get_user_model
 from lists.models import List
 from lists.forms import ItemForm, ExistingListItemForm
+
+User = get_user_model()
 
 
 class HomePageView(FormView):
@@ -33,5 +36,18 @@ class NewListView(CreateView):
         form.save(for_list=list_)
         return redirect(list_)
 
+
 def my_lists(request, email):
-    return render(request, "my_lists.html")
+    owner = User.objects.get(email=email)
+    return render(request, "my_lists.html", {"owner": owner})
+
+def new_list(request):
+    form = ItemForm(data=request.POST)
+    if form.is_valid():
+        list_ = List()
+        list_.owner = request.user
+        list_.save()
+        form.save(for_list=list_)
+        return redirect(list_)
+    else:
+        return render(request, "home.html", {"form": form})
